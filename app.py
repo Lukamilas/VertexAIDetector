e39f3b3f96f7bf24a39afe55255ca427e433cb3d
Lukamilas <lukamilas149@gmail.com>
2026-08-30 10:14:35 -0400

    Remove unused nltk import from app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from test_data import test_human, test_ai
 import random
-import nltk
 from nltk.corpus import brown
 
 # TRAINING DATA
