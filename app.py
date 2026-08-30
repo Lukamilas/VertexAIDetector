f39003ac86d153862150f27cc21cd6f372ea1f56
Lukamilas <lukamilas149@gmail.com>
2026-08-30 10:05:51 -0400

    Add random and nltk imports to app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from test_data import test_human, test_ai
+import random
+import nltk
 from nltk.corpus import brown
 
 # TRAINING DATA
